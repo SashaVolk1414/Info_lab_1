192fed1a420b2de2bca9a352514d3d3ad2cb38c0
SashaVolk1414 <127424701+SashaVolk1414@users.noreply.github.com>
2025-09-19 09:31:26 +0400

    Add files via upload
    Лаба 1. Задание 1, задача 1

---
+++ Prog1(2).cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+//Задача 1 
+
+bool Proverka(double v) {
+    if(v>=0){ 
+        return true;
+    }
+    return false;
+        
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double v = 0;
+    cout << "Напишите вашу скорость в км/ч: ";
+    cin >> v;
+    if (Proverka(v)) {
+        if (v < 60) {
+            cout << "Скорость в пределах нормы " << endl;
+        }
+        else if (v >= 60 or v <= 80) {
+            cout << "Вы приближаетесь к превышению скорости" << endl;
+        }
+        else {
+            cout << "Превышение скорости!" << endl;
+        }
+    }
+    else {
+        cout << "Вы ввели неверный формат скорости! " << endl;
+    }
+   
+    return 0;
+}
+
+//Задача  2 
+/*
+bool Proverka(int f) {
+    if (f < 0) {
+        return false;
+    }
+    return true;
+
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int f = 0;
+    cout << "Введите количество машин: ";
+    cin >> f;
+    int res = 1,l=1;
+    if (Proverka(f)) {
+        while (l <= f) {
+            res = res * l;
+            l++;
+        }
+        cout << "Производительность машин на фабрике: "<<res;
+    }
+    else {
+        cout << "Кол-во машин не может быть отрицательным числом! Введите правильно значение.";
+    }
+
+    return 0;
+}*/
